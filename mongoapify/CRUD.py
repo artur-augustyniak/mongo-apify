6076a8b2cf2a49a03f3b16867c66875853269c7d
Artur Augustyniak <artur@aaugustyniak.pl>
2021-10-04 14:43:00 +0200

    bulk operations

--- mongoapify/CRUD.py
+++ mongoapify/CRUD.py
@@ -7,6 +7,8 @@ from bson.objectid import ObjectId
 from datetime import datetime as dt
 import logging
 
+BULK_SIZE = 500
+
 logger = logging.getLogger(__name__)
 
 
@@ -33,21 +35,21 @@ class MongoProvider(object):
         def parse_dir(e):
             if len(e) > 1:
                 if e.startswith("+"):
-                    return ((e[1:], pymongo.ASCENDING))
+                    return (e[1:], pymongo.ASCENDING)
                 elif e.startswith("-"):
-                    return ((e[1:], pymongo.DESCENDING))
+                    return (e[1:], pymongo.DESCENDING)
                 else:
                     return None
             else:
                 return None
-        lst = list(filter(lambda i: i is not None, map(
-            parse_dir,
-            sort_list
-        )))
+
+        lst = list(filter(lambda i: i is not None, map(parse_dir, sort_list)))
         if lst:
             return lst
         else:
-            return [('_id', pymongo.DESCENDING), ]
+            return [
+                ("_id", pymongo.DESCENDING),
+            ]
 
     @staticmethod
     def _create_mongo_filter_dict(filter_list, whole_word, ignore_case, force_and):
@@ -69,15 +71,23 @@ class MongoProvider(object):
                     return {field: val}
                 else:
                     if whole_word:
-                        return {field: {'$regex': '^%s$' % (val), '$options': 'i' if ignore_case else ''}}
+                        return {
+                            field: {
+                                "$regex": "^%s$" % (val),
+                                "$options": "i" if ignore_case else "",
+                            }
+                        }
                     else:
-                        return {field: {'$regex': val, '$options': 'i' if ignore_case else ''}}
+                        return {
+                            field: {
+                                "$regex": val,
+                                "$options": "i" if ignore_case else "",
+                            }
+                        }
+
             return thunk
 
-        flist = list(map(
-            make_query_dict_fun(),
-            tuples
-        ))
+        flist = list(map(make_query_dict_fun(), tuples))
         if flist:
             filter_dict = {"$and" if force_and else "$or": list(flist)}
             logger.debug("mongo filter %s" % (json.dumps(filter_dict)))
@@ -87,7 +97,7 @@ class MongoProvider(object):
 
     @staticmethod
     def _create_mongo_timerange_filters(created_at_lst, updated_at_lst):
-        oplist = ['lte', 'gte', 'eq', 'ne']
+        oplist = ["lte", "gte", "eq", "ne"]
 
         def parse_dt(dt_str):
             try:
@@ -102,58 +112,57 @@ class MongoProvider(object):
             for t in tuples:
                 dt_obj = parse_dt(t[1])
                 if t[0] in oplist and dt_obj is not None:
-                    query_dict['$%s' % (t[0])] = dt_obj
+                    query_dict["$%s" % (t[0])] = dt_obj
             return query_dict
 
         return {
-            'created_at': make_query_dict(created_at_lst),
-            'updated_at': make_query_dict(updated_at_lst)
+            "created_at": make_query_dict(created_at_lst),
+            "updated_at": make_query_dict(updated_at_lst),
         }
 
     def get_all(self, q={}) -> dict:
-        filtering = q.get('filtering', [])
-        ignore_case = q.get('ignore_case', False)
-        whole_word = q.get('whole_word', True)
-        force_and = q.get('force_and', False)
-        shape_mode = q.get('shape_mode', None)
-        limit = q.get('limit', 20)
-        offset = q.get('offset', 0)
-        sort = q.get('sort', ['-_id'])
-        created_at = q.get('created_at', [])
-        updated_at = q.get('updated_at', [])
-        shape = q.get('shape', None)
+        filtering = q.get("filtering", [])
+        ignore_case = q.get("ignore_case", False)
+        whole_word = q.get("whole_word", True)
+        force_and = q.get("force_and", False)
+        shape_mode = q.get("shape_mode", None)
+        limit = q.get("limit", 20)
+        offset = q.get("offset", 0)
+        sort = q.get("sort", ["-_id"])
+        created_at = q.get("created_at", [])
+        updated_at = q.get("updated_at", [])
+        shape = q.get("shape", None)
         if shape is not None:
             shape = {k: int(shape_mode) for k in shape}
         time_fltr = MongoProvider._create_mongo_timerange_filters(
-            created_at, updated_at)
+            created_at, updated_at
+        )
         fltr = MongoProvider._create_mongo_filter_dict(
-            filtering, whole_word, ignore_case, force_and)
-        if time_fltr['created_at']:
-            fltr['created_at'] = time_fltr['created_at']
-        if time_fltr['updated_at']:
-            fltr['updated_at'] = time_fltr['updated_at']
-        items = self.mycol.find(fltr, shape)\
-            .skip(offset)\
-            .limit(limit)\
+            filtering, whole_word, ignore_case, force_and
+        )
+        if time_fltr["created_at"]:
+            fltr["created_at"] = time_fltr["created_at"]
+        if time_fltr["updated_at"]:
+            fltr["updated_at"] = time_fltr["updated_at"]
+        items = (
+            self.mycol.find(fltr, shape)
+            .skip(offset)
+            .limit(limit)
             .sort(MongoProvider._create_mongo_sort_dict(sort))
+        )
         count = items.count()
         items = list(items)
         items = json.loads(JSONEncoder().encode(items))
-        return {
-            'count': count,
-            'results': items
-
-        }
+        return {"count": count, "results": items}
 
     def create(self, payload={}):
-        pld = payload.get('payload', None)
+        pld = payload.get("payload", None)
         if pld is None:
-            raise RuntimeError(
-                "create - no payload key")
+            raise RuntimeError("create - no payload key")
         pld.pop("_id", None)
         creation_time = dt.now()
-        pld['created_at'] = creation_time
-        pld['updated_at'] = creation_time
+        pld["created_at"] = creation_time
+        pld["updated_at"] = creation_time
         try:
             self.mycol.insert_one(pld)
             return json.loads(JSONEncoder().encode(pld)), False
@@ -164,120 +173,50 @@ class MongoProvider(object):
             existing = self.mycol.find_one(idx_query)
             return json.loads(JSONEncoder().encode(existing)), True
 
-
-    def create_bulk(self, payload={}):
-        pld = payload.get('payload', None)
-        if type(pld) is not list:
-            raise RuntimeError(
-                "create - no payload key or payload is not an array")
-        n_inserted = 0
-        sent = len(pld)
-        errors = []
-
-        for doc in pld:
-            doc.pop("_id", None)
-            creation_time = dt.now()
-            doc['created_at'] = creation_time
-            doc['updated_at'] = creation_time
-        try:
-            res = self.mycol.insert_many(pld, ordered=False)
-            n_inserted = len(res.inserted_ids)
-        except pymongo.errors.BulkWriteError as bwe:
-            n_inserted = bwe.details.get("nInserted", 0)
-            errors = filter(
-                lambda i: i is not None,
-                map(lambda item: {
-                        "key_value" : item.get("keyValue", None), 
-                        "key_pattern" : item.get("keyPattern", None), 
-                        "error_msg" : item.get("errmsg", None)
-                    },
-                    bwe.details.get("writeErrors", [])
-                ))
-            
-        res = {
-            "sent" : sent,
-            "processed" : n_inserted,
-            "errors": list(errors)
-        }
-        return json.loads(JSONEncoder().encode(res)), n_inserted > 0
-
-
-    
-
     def get_one(self, _id) -> dict:
         query = {"_id": ObjectId(_id)}
         item = self.mycol.find_one(query)
         item = JSONEncoder().encode(item)
         return json.loads(item)
 
-    def update(self, _id, upload_payload={}) -> dict:
+    def update(self, _id, payload={}) -> dict:
+
         orig_entry = self.get_one(_id)
         if orig_entry is None:
             return None
+
+        upload_payload = payload.get("patch_payload", None)
+        if type(upload_payload) is not dict:
+            raise RuntimeError(
+                "update - no patch_payload key or patch_payload is not an object"
+            )
         query = {"_id": ObjectId(_id)}
+
         upload_payload.pop("_id", None)
         upload_payload.pop("created_at", None)
         for ui in self.uq_indices:
             upload_payload.pop(ui, None)
         update_time = dt.now()
-        upload_payload['updated_at'] = update_time
+        upload_payload["updated_at"] = update_time
         new_values = {"$set": upload_payload}
         _ = self.mycol.update_one(query, new_values)
-        upload_payload['updated_at'] = str(upload_payload['updated_at'])
+        upload_payload["updated_at"] = str(upload_payload["updated_at"])
         ret_dict = {}
-        ret_dict['entry'] = orig_entry
-        ret_dict['patch'] = upload_payload
+        ret_dict["entry"] = orig_entry
+        ret_dict["patch"] = upload_payload
         # changed_fields = []
         # def iterdict(d):
-        #     for k,v in d.items():        
+        #     for k,v in d.items():
         #         if isinstance(v, dict):
         #             iterdict(v)
-        #         else:            
+        #         else:
         #             print("$" * 80)
         #             print (k,":",v)
-        #             if k in orig_entry and 
+        #             if k in orig_entry and
 
         # iterdict(upload_payload)
-        
+
         return ret_dict
-
-    def update_bulk(self, payload={}):
-        bulk write!
-        from pprint import pprint as pp
-        pld = payload.get('payload', None)
-        if type(pld) is not list:
-            raise RuntimeError(
-                "create - no payload key or payload is not an array")
-        n_inserted = 0
-        sent = len(pld)
-        errors = []
-
-        for doc in pld:
-            doc.pop("_id", None)
-            creation_time = dt.now()
-            doc['created_at'] = creation_time
-            doc['updated_at'] = creation_time
-        try:
-            res = self.mycol.insert_many(pld, ordered=False)
-            n_inserted = len(res.inserted_ids)
-        except pymongo.errors.BulkWriteError as bwe:
-            n_inserted = bwe.details.get("nInserted", 0)
-            errors = filter(
-                lambda i: i is not None,
-                map(lambda item: {
-                        "key_value" : item.get("keyValue", None), 
-                        "key_pattern" : item.get("keyPattern", None), 
-                        "error_msg" : item.get("errmsg", None)
-                    },
-                    bwe.details.get("writeErrors", [])
-                ))
-            
-        res = {
-            "sent" : sent,
-            "processed" : n_inserted,
-            "errors": list(errors)
-        }
-        return json.loads(JSONEncoder().encode(res)), n_inserted > 0
 
     def delete(self, _id) -> dict:
         query = {"_id": ObjectId(_id)}
@@ -286,3 +225,76 @@ class MongoProvider(object):
             return {"message": "Item deleted"}
         else:
             None
+
+    def create_bulk(self, payload={}):
+        pld = payload.get("payload", None)
+        if type(pld) is not list:
+            raise RuntimeError(
+                "create_bulk - no payload key or payload is not an array"
+            )
+        n_inserted = 0
+        sent = len(pld)
+        errors = []
+
+        for doc in pld:
+            doc.pop("_id", None)
+            creation_time = dt.now()
+            doc["created_at"] = creation_time
+            doc["updated_at"] = creation_time
+        try:
+            res = self.mycol.insert_many(pld, ordered=False)
+            n_inserted = len(res.inserted_ids)
+        except pymongo.errors.BulkWriteError as bwe:
+            n_inserted = bwe.details.get("nInserted", 0)
+            errors = filter(
+                lambda i: i is not None,
+                map(
+                    lambda item: {
+                        "key_value": item.get("keyValue", None),
+                        "key_pattern": item.get("keyPattern", None),
+                        "error_msg": item.get("errmsg", None),
+                    },
+                    bwe.details.get("writeErrors", []),
+                ),
+            )
+
+        res = {"sent": sent, "processed": n_inserted, "errors": list(errors)}
+        return json.loads(JSONEncoder().encode(res)), n_inserted > 0
+
+    def update_bulk(self, payload={}):
+        up_plds = payload.get("patch_payload", None)
+        if type(up_plds) is not list:
+            raise RuntimeError(
+                "update_bulk - no patch_payload key or payload is not an array"
+            )
+        sent = len(up_plds)
+        errors = []
+        bulk = self.mycol.initialize_unordered_bulk_op()
+        counter = 0
+        n_matched = 0
+        for patch in up_plds:
+            try:
+                bulk.find({"_id": ObjectId(patch.get("_id"))}).update(
+                    {"$set": patch.get("patch", {})}
+                )
+                counter += 1
+            except Exception as e:
+                errors.append(
+                    {
+                        "key_value": {"_id": patch.get("_id")},
+                        "key_pattern": {},
+                        "error_msg": str(e),
+                    }
+                )
+            if counter % BULK_SIZE == 0 and counter > 0:
+                r_par = bulk.execute()
+                n_matched += r_par.get("nMatched", 0)
+
+                bulk = self.mycol.initialize_ordered_bulk_op()
+
+        if counter % BULK_SIZE != 0:
+            r_tail = bulk.execute()
+            n_matched += r_tail.get("nMatched", 0)
+
+        res = {"sent": sent, "processed": n_matched, "errors": list(errors)}
+        return json.loads(JSONEncoder().encode(res)), n_matched > 0
--- mongoapify/swagger.py
+++ mongoapify/swagger.py
@@ -1,129 +1,203 @@
 import yaml
 
 
-DEFAULT_API_RESP = {'description': 'INFO or ERROR',
-                    'schema': {'properties': {'message': {'type': 'string'},
-                                              'status': {'type': 'integer'}},
-                               'type': 'object'}}
-
-GET_SINGLE_PARAMS = [{'description': 'Item ID.',
-                      'in': 'path',
-                      'name': '_id',
-                      'required': True,
-                      'type': 'string'}]
+DEFAULT_API_RESP = {
+    "description": "INFO or ERROR",
+    "schema": {
+        "properties": {"message": {"type": "string"}, "status": {"type": "integer"}},
+        "type": "object",
+    },
+}
 
 
-GET_ALL_PARAMS = [{'default': 20,
-                   'description': 'The numbers of items to return.',
-                   'in': 'query',
-                   'maximum': 100,
-                   'minimum': 1,
-                   'name': 'limit',
-                   'required': False,
-                   'type': 'integer'},
-                  {'default': 0,
-                   'description': 'The number of items to skip before starting to collect the result set.',
-                   'in': 'query',
-                   'minimum': 0,
-                   'name': 'offset',
-                   'required': False,
-                   'type': 'integer'},
-                  {'collectionFormat': 'csv',
-                   'default': ['-_id'],
-                   'in': 'query',
-                   'items': {'type': 'string'},
-                   'name': 'sort',
-                   'type': 'array',
-                   'uniqueItems': True},
-                  {'default': False,
-                   'description': 'Switch for search case sensitivity setting',
-                   'in': 'query',
-                   'name': 'ignore_case',
-                   'required': False,
-                   'type': 'boolean'},
-                  {'default': True,
-                   'description': 'Switch for search whole word setting',
-                   'in': 'query',
-                   'name': 'whole_word',
-                   'required': False,
-                   'type': 'boolean'},
-                  {'default': False,
-                   'description': 'Force AND as filer items operator (default is OR)',
-                   'in': 'query',
-                   'name': 'force_and',
-                   'required': False,
-                   'type': 'boolean'},
-                  {'collectionFormat': 'csv',
-                   'in': 'query',
-                   'items': {'type': 'string'},
-                   'name': 'filtering',
-                   'type': 'array'},
-                    {'default': True,
-                   'description': 'Switch mode for shape list - include only(True)/exclude_only(False)',
-                   'in': 'query',
-                   'name': 'shape_mode',
-                   'required': False,
-                   'type': 'boolean'},
-                  {'collectionFormat': 'csv',
-                   'in': 'query',
-                   'items': {'type': 'string'},
-                   'name': 'shape',
-                   'type': 'array'},
-                  {'in': 'query',
-                   'items': {'type': 'string'},
-                   'maxItems': 4,
-                   'minItems': 2,
-                   'name': 'created_at',
-                   'type': 'array'},
-                  {'in': 'query',
-                   'items': {'type': 'string'},
-                   'maxItems': 4,
-                   'minItems': 2,
-                   'name': 'updated_at',
-                   'type': 'array'}]
+BULK_API_RESP_DATA = {
+    "properties": {
+        "errors": {
+            "items": {
+                "properties": {
+                    "error_msg": {"type": "string"},
+                    "key_pattern": {"type": "object"},
+                    "key_value": {"type": "object"},
+                },
+                "type": "object",
+            },
+            "type": "array",
+        },
+        "processed": {"type": "integer"},
+        "sent": {"type": "integer"},
+    },
+    "type": "object",
+}
+
+
+BULK_API_SUCCESS_RESP = {
+    "description": "Returned if any of requests were successful",
+    "schema": BULK_API_RESP_DATA,
+}
+BULK_API_FALIURE_RESP = {
+    "description": "Returned if all of requests were unsuccessful",
+    "schema": BULK_API_RESP_DATA,
+}
+
+
+GET_SINGLE_PARAMS = [
+    {
+        "description": "Item ID.",
+        "in": "path",
+        "name": "_id",
+        "required": True,
+        "type": "string",
+    }
+]
+
+
+GET_ALL_PARAMS = [
+    {
+        "default": 20,
+        "description": "The numbers of items to return.",
+        "in": "query",
+        "maximum": 100,
+        "minimum": 1,
+        "name": "limit",
+        "required": False,
+        "type": "integer",
+    },
+    {
+        "default": 0,
+        "description": "The number of items to skip before starting to collect the result set.",
+        "in": "query",
+        "minimum": 0,
+        "name": "offset",
+        "required": False,
+        "type": "integer",
+    },
+    {
+        "collectionFormat": "csv",
+        "default": ["-_id"],
+        "in": "query",
+        "items": {"type": "string"},
+        "name": "sort",
+        "type": "array",
+        "uniqueItems": True,
+    },
+    {
+        "default": False,
+        "description": "Switch for search case sensitivity setting",
+        "in": "query",
+        "name": "ignore_case",
+        "required": False,
+        "type": "boolean",
+    },
+    {
+        "default": True,
+        "description": "Switch for search whole word setting",
+        "in": "query",
+        "name": "whole_word",
+        "required": False,
+        "type": "boolean",
+    },
+    {
+        "default": False,
+        "description": "Force AND as filer items operator (default is OR)",
+        "in": "query",
+        "name": "force_and",
+        "required": False,
+        "type": "boolean",
+    },
+    {
+        "collectionFormat": "csv",
+        "in": "query",
+        "items": {"type": "string"},
+        "name": "filtering",
+        "type": "array",
+    },
+    {
+        "default": True,
+        "description": "Switch mode for shape list - include only(True)/exclude_only(False)",
+        "in": "query",
+        "name": "shape_mode",
+        "required": False,
+        "type": "boolean",
+    },
+    {
+        "collectionFormat": "csv",
+        "in": "query",
+        "items": {"type": "string"},
+        "name": "shape",
+        "type": "array",
+    },
+    {
+        "in": "query",
+        "items": {"type": "string"},
+        "maxItems": 4,
+        "minItems": 2,
+        "name": "created_at",
+        "type": "array",
+    },
+    {
+        "in": "query",
+        "items": {"type": "string"},
+        "maxItems": 4,
+        "minItems": 2,
+        "name": "updated_at",
+        "type": "array",
+    },
+]
 
 
 def insert_paths_parts(paths):
-    '''
+    """
     pyyaml loose reference without:
     yaml.load(yaml.dump(GET_ALL_PARAMS))
-    '''
+    """
     for path, methods in paths.items():
-        GET = methods.get('get', {})
-        if "}" not in path and not GET.get('parameters'):
-            GET['parameters'] = yaml.load(yaml.dump(GET_ALL_PARAMS))
 
-        if "}" in path and not GET.get('parameters'):
-            GET['parameters'] = yaml.load(yaml.dump(GET_SINGLE_PARAMS))
-        DELETE = methods.get('delete', {})
-        if "}" in path and not DELETE.get('parameters'):
-            DELETE['parameters'] = yaml.load(yaml.dump(GET_SINGLE_PARAMS))
+        if "/bulk" in path:
+            for method, definition in methods.items():
+                if method in ["patch", "post"]:
+                    if not definition.get("responses"):
+                        definition["responses"] = {}
+                        definition["responses"]["201"] = yaml.load(
+                            yaml.dump(BULK_API_SUCCESS_RESP)
+                        )
+                        definition["responses"]["400"] = yaml.load(
+                            yaml.dump(BULK_API_FALIURE_RESP)
+                        )
+
+        GET = methods.get("get", {})
+        if "}" not in path and not GET.get("parameters"):
+            GET["parameters"] = yaml.load(yaml.dump(GET_ALL_PARAMS))
+
+        if "}" in path and not GET.get("parameters"):
+            GET["parameters"] = yaml.load(yaml.dump(GET_SINGLE_PARAMS))
+        DELETE = methods.get("delete", {})
+        if "}" in path and not DELETE.get("parameters"):
+            DELETE["parameters"] = yaml.load(yaml.dump(GET_SINGLE_PARAMS))
 
         for method, definition in methods.items():
-            if not definition.get('produces'):
-                definition['produces'] = ['application/json']
-            if method in ['patch', 'post']:
-                if not definition.get('consumes'):
-                    definition['consumes'] = ['application/json']
-            if not definition.get('responses', {}).get('default'):
+            if not definition.get("produces"):
+                definition["produces"] = ["application/json"]
+            if method in ["patch", "post"]:
+                if not definition.get("consumes"):
+                    definition["consumes"] = ["application/json"]
+            if not definition.get("responses", {}).get("default"):
                 d = paths[path][method]
-                if not d.get('responses'):
-                    d['responses'] = {}
-                d['responses']['default'] = yaml.load(
-                    yaml.dump(DEFAULT_API_RESP))
+                if not d.get("responses"):
+                    d["responses"] = {}
+                d["responses"]["default"] = yaml.load(yaml.dump(DEFAULT_API_RESP))
 
 
 def complete_yaml(yaml_file_path, api_version, host, base_path, scheme):
     with open(yaml_file_path) as f:
         y = yaml.load(f)
-        insert_paths_parts(y.get('paths', {}))
-        y['swagger'] = '2.0'
-        y['host'] = host
-        y['basePath'] = base_path.replace("-VER-", api_version)
-        y['schemes'] = [scheme]
-        if y.get('info'):
-            y['info']['version'] = api_version
-        y['securityDefinitions'] = {'API-Key': {'in': 'header',
-                                                'name': 'X-API-Key',
-                                                'type': 'apiKey'}}
+        insert_paths_parts(y.get("paths", {}))
+        y["swagger"] = "2.0"
+        y["host"] = host
+        y["basePath"] = base_path.replace("-VER-", api_version)
+        y["schemes"] = [scheme]
+        if y.get("info"):
+            y["info"]["version"] = api_version
+        y["securityDefinitions"] = {
+            "API-Key": {"in": "header", "name": "X-API-Key", "type": "apiKey"}
+        }
         return yaml.dump(y)
